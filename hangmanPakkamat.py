de7461ce6c544d1d7c39fcf93f155e4802a0f398
Pakkamat <71426393+Pakkamat@users.noreply.github.com>
2020-11-18 10:52:51 +0700

    Add files via upload

---
+++ hangmanPakkamat.py
@@ -0,0 +1,30 @@
+import random
+aaa = ["hangman", "teacher", "helloworld"]
+n = random.randint(0, (len(aaa) - 1))
+word = aaa[n]
+y = ''
+turns = 8
+while turns > 0:
+    failed = 0
+    # Print the words that contain in word put in y
+    for i in word:
+        if i in y:
+            print(i, end=' ')
+        else:
+            print("_", end=' ')
+            failed += 1
+    # If u have all word == Win
+    if failed == 0:
+        print(">> You survived !")
+        break
+    x = input("Input a letter: > ")
+    # put x in y
+    y += x
+    # x not in word turns or times -1
+    if x not in word:
+        turns -= 1
+        print("No such letter in the word")
+    #print("You have", + turns, 'lives')
+    # if turn = 0 >>> lose
+    if turns == 0:
+        print("You hanged")
